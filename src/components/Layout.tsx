33c24dfc4efd29470fdbc770506940fa3deac838
unknown <inriveronika17@gmail.com>
2023-01-23 21:36:23 +0700

    feat: add component layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+
+import Navbar from "./Navbar";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="w-full h-screen flex flex-col overflow-auto">
+      <Navbar />
+      <div className="h-full w-full overflow-auto">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
