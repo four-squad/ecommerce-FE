68ece49fc4eb6d03a9c5c97e40c0cebc4e89fac4
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-23 17:56:08 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className='text-5xl text-red-500'>Landing page</div>
+  )
+}
+
+export default index
---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom' 
+
+import LandingPage from 'pages'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LandingPage />,
+    },
+])
+const index = () => {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default index
