255e96499ad24f0d06c8c60e497d761df28e15d6
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-01-24 11:42:06 +0700

    feat: add custom input text area

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,4 +1,4 @@
-import { FC, InputHTMLAttributes } from 'react'
+import { FC, InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
 
 interface PropsInput extends InputHTMLAttributes<HTMLInputElement> {
     parentSet?: string
@@ -6,6 +6,12 @@ interface PropsInput extends InputHTMLAttributes<HTMLInputElement> {
     inputSet?: string;
     labelSet?: string;
 }
+interface PropsTextArea extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+    parentSet?: string
+    label?: string;
+    inputSet?: string;
+    labelSet?: string;
+  }
 
 export const CustomInput: FC<PropsInput> = ({parentSet, label, inputSet, labelSet, ...props}) => {
     return (
@@ -15,6 +21,20 @@ export const CustomInput: FC<PropsInput> = ({parentSet, label, inputSet, labelSe
                 className={`input input-bordered w-full bg-white ${inputSet}`}
                 {...props}
             />
+        </div>
+    )
+}
+
+export const TextArea: FC<PropsTextArea> = ({parentSet, label, inputSet, labelSet, ...props}) => {
+    return (
+        <div className={`mb-3 ${parentSet}`}>
+            <label className={`${labelSet}`}>{label}</label>
+            <textarea
+                className={`input input-bordered w-full bg-white ${inputSet}`}
+                {...props}
+            >
+
+            </textarea>
         </div>
     )
 }
