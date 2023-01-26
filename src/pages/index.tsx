16a0c72ed8c195e8428fb13166e09d10a69e1f5e
unknown <inriveronika17@gmail.com>
2023-01-26 09:34:50 +0700

    feat: add to cart handler

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
-import { AiOutlineLeftCircle, AiOutlineRightCircle } from "react-icons/ai";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 
-import Layout from "components/Layout";
 import { CardHome } from "components/Card";
+import Layout from "components/Layout";
 
-interface ProductType {
-  id: number;
-  title: string;
-  image: string;
-  price: number;
-}
+import { ProductType } from "utils/type";
 
 const index = () => {
   const [product, setProduct] = useState<ProductType[]>([]);
@@ -23,13 +17,10 @@ const index = () => {
     axios
       .get("https://remotecareer.tech/products")
       .then((res) => {
-        console.log("yey", res);
         const { data } = res.data;
         setProduct(data);
       })
-      .catch((err) => {
-        console.log("nay", err);
-      });
+      .catch((err) => {});
   }
   useEffect(() => {
     fetchData();
@@ -76,20 +67,6 @@ const index = () => {
               );
             })}
           </div>
-          {/* <div className="flex w-full my-10 ">
-            <div className='flex w-full justify-end mr-5 text-base-100 dark:text-black'>
-              <div className='mx-2'>
-                <AiOutlineLeftCircle size={35}
-                // onClick={() => prevPage()}
-                />
-              </div>
-              <div className='mx-2'>
-                <AiOutlineRightCircle size={35}
-                // onClick={() => nextPage()}
-                />
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </Layout>
